e79f6b322109081ff3645c7677f210670b370d39
RamitShapkota <shapkotaramit47@gmail.com>
2026-09-15 16:10:03 +0545

    feat: add event loop example demonstrating setImmediate, setTimeout, Promise, and nextTick behavior

---
+++ 09_libuv & Event Loop/event-loop-4.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+setImmediate(() => console.log('setImmediate'));
+
+setTimeout(() => {
+    console.log('Timer Expired');
+}, 10);
+
+Promise.resolve()
+    .then(() => console.log('promise'));
+
+fs.readFile("./file.txt", "utf8", (res) => {
+    console.log('File Reading CB');
+});
+
+
+process.nextTick(() => {
+    process.nextTick(() => console.log('inner nextTick'));
+    console.log('nextTick');
+    
+});
+
+
+console.log('Last line of the file.');
+
+/**OUTPUT
+ * Last line of the file
+ * nextTick
+ * inner nextTick
+ * promise
+ * Timer Expired
+ * setImmediate
+ * File Reading CB
+ */
