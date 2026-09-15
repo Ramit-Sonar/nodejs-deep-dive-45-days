a07b59582721710efbf40a933e8c8370456b7569
RamitShapkota <shapkotaramit47@gmail.com>
2026-09-15 15:00:39 +0545

    feat: add initial event loop examples and a text file for demonstration

---
+++ 09_libuv & Event Loop/event-loop-1.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const a = 100;
+
+setImmediate(() => console.log('setImmediate'));
+
+fs.readFile("./file.txt", "utf8", (res) => {
+    console.log('File Reading CB');
+});
+
+setTimeout(() => {
+    console.log('Timer Expired');
+},10);
+
+function prindA() {
+    console.log('a=', a);
+}
+
+prindA();
+console.log('Last line of the file.');
+
+
